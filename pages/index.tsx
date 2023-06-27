f2556d5492194feb37bad33d77090ebad7225e0c
niyobern <niyobern@outlook.com>
2023-06-28 01:44:15 +0200

    changed import of contentjson

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ import { readFile } from 'fs/promises';
 export const getServerSideProps = handle({
   async get({ cookies }: any) {
     const lang = cookies.get("lang")
-    const data = lang == "gb" ? await readFile("/public/contents.json", {encoding: "utf-8"}): await readFile("/public/contentsKiny.json", {encoding: "utf-8"})
+    const data = lang == "gb" ? await readFile("/public/contents.json", {encoding: "utf-8"}): await readFile("../components/contentsKiny.json", {encoding: "utf-8"})
     return json({data, lang});
   },
   async post({ req: { body }, cookies}: any) {
