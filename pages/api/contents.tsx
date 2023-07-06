fc19b5dc4d9abe59ad01684bd4179c20d03dc6da
niyobern <niyobern@outlook.com>
2023-07-06 23:14:20 +0200

    added debugger on api/content

--- pages/api/contents.tsx
+++ pages/api/contents.tsx
@@ -1,9 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next"
-import { readFile, writeFile } from "fs/promises"
-import path from "path"
 import { kv } from "@vercel/kv"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse){
+    console.log(req.body.data)
     if (req.body.lang == "gb"){
         const data: any = await kv.hgetall("gb");
         data[req.body.id] = req.body.data;
