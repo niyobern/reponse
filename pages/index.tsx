f1f446bb5110b9da8130632a93d99683eaffe48e
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-07-19 00:06:51 +0200

    Update index.tsx by changing to getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import Layout from '../components/layout';
 import { handle, json, redirect } from 'next-runtime';
 import { kv } from '@vercel/kv';
 
-export const getServerSideProps = handle({
+export const getStaticProps = handle({
   async get({ cookies }: any) {
     const lang = cookies.get("lang") || "gb"
     let  result = await kv.hgetall(lang)
