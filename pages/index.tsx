62d413d6b40b5665d6150a85423b73b3e01d5bf4
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-07-19 00:36:48 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,18 +6,18 @@ import Layout from '../components/layout';
 import { handle, json, redirect } from 'next-runtime';
 import { kv } from '@vercel/kv';
 
-export const getServerSideProps = handle({
-  async get({ cookies }: any) {
-    const lang = cookies.get("lang") || "gb"
-    // let  result = await kv.hgetall(lang)
-    // const data = JSON.stringify(result)
-    return json({lang});
-  },
-  async post({ req: { body }, cookies}: any) {
-    cookies.set("lang", body.lang)
-    return json({});
-  },
-});
+// export const getServerSideProps = handle({
+//   async get({ cookies }: any) {
+//     const lang = cookies.get("lang") || "gb"
+//     // let  result = await kv.hgetall(lang)
+//     // const data = JSON.stringify(result)
+//     return json({lang});
+//   },
+//   async post({ req: { body }, cookies}: any) {
+//     cookies.set("lang", body.lang)
+//     return json({});
+//   },
+// });
 
 export async function getStaticProps() {
   const rw = await kv.hgetall("rw")
@@ -25,7 +25,8 @@ export async function getStaticProps() {
   return {props: { data: {rw: rw, gb: gb} }}
 }
 
-export default function Home({ data, lang }: any) {
+export default function Home({ data }: any) {
+  const lang = "gb"
   const router = useRouter()
   return (
     <Layout language={lang}>
