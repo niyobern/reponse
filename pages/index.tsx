abbb2fd7122b1443df2bb019dd2079f2ef3c10ab
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-07-19 00:27:13 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,12 @@ import Layout from '../components/layout';
 import { handle, json, redirect } from 'next-runtime';
 import { kv } from '@vercel/kv';
 
-export const getStaticProps = handle({
+export const getServerSideProps = handle({
   async get({ cookies }: any) {
     const lang = cookies.get("lang") || "gb"
-    let  result = await kv.hgetall(lang)
-    const data = JSON.stringify(result)
-    return json({data, lang});
+    // let  result = await kv.hgetall(lang)
+    // const data = JSON.stringify(result)
+    return json({lang});
   },
   async post({ req: { body }, cookies}: any) {
     cookies.set("lang", body.lang)
@@ -19,14 +19,20 @@ export const getStaticProps = handle({
   },
 });
 
+export const getStaticProps = async => {
+  const rw = await kv.hgetall("rw")
+  const gb = kv.hgetall("gb")
+  return {props: { data: {rw: rw, gb: gb} }}
+}
+
 export default function Home({ data, lang }: any) {
   const router = useRouter()
   return (
     <Layout language={lang}>
       <div>
-        <Hero data={data}/>
-        <Services data={data}/>
-        <MissionVission data={data}/>
+        <Hero data={data[lang]}/>
+        <Services data={data[lang]}/>
+        <MissionVission data={data[lang]}/>
       </div>
     </Layout>
   );
