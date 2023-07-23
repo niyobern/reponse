eaf454606923cae2f6e77e73640c02075a2f2a3e
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-07-23 08:29:08 +0200

    Update team.tsx

--- components/team.tsx
+++ components/team.tsx
@@ -14,7 +14,7 @@ export default function TeamComponent(){
             <div className="flex flex-col md:flex-row gap-4 md:gap-0 flex-wrap justify-center">
            {/* <!-- Team member card --> */}
           <div className="w-full md:w-1/5 bg-white rounded overflow-hidden shadow-lg md:m-4 flex flex-col justify-between">
-            <Image height={100} width={100} className="w-full rounded-full px-2 md:px-4" src={avatar1} alt="Team member photo"></Image>
+            <Image height={100} width={100} className="w-full h-full rounded-full px-2 md:px-4" src={avatar1} alt="Team member photo"></Image>
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">John Doe</div>
               <p className="text-gray-700 text-base">Web developer and designer</p>
@@ -27,7 +27,7 @@ export default function TeamComponent(){
             </div>
           </div>
           <div className="w-full md:w-1/5 bg-white rounded overflow-hidden shadow-lg md:m-4 flex flex-col justify-between">
-            <Image height={100} width={100} className="w-full rounded-full px-2 md:px-4" src={avatar2} alt="Team member photo"></Image>
+            <Image height={100} width={100} className="w-full h-full rounded-full px-2 md:px-4" src={avatar2} alt="Team member photo"></Image>
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">Jane Doe</div>
               <p className="text-gray-700 text-base">Data analyst and machine learning engineer</p>
@@ -40,7 +40,7 @@ export default function TeamComponent(){
             </div>
           </div>
           <div className="w-full md:w-1/5 bg-white rounded overflow-hidden shadow-lg md:m-4 flex flex-col justify-between">
-            <Image height={100} width={100} className="w-full rounded-full px-2 md:px-4" src={avatar3} alt="Team member photo"></Image>
+            <Image height={100} width={100} className="w-full h-full rounded-full px-2 md:px-4" src={avatar3} alt="Team member photo"></Image>
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">Jane Doe</div>
               <p className="text-gray-700 text-base">Data analyst and machine learning engineer</p>
@@ -53,7 +53,7 @@ export default function TeamComponent(){
             </div>
           </div>                
           <div className="w-full md:w-1/5 bg-white rounded overflow-hidden shadow-lg md:m-4 flex flex-col justify-between">
-            <Image height={100} width={100} className="w-full rounded-full px-2 md:px-4" src={avatar4} alt="Team member photo"></Image>
+            <Image height={100} width={100} className="w-full h-full rounded-full px-2 md:px-4" src={avatar4} alt="Team member photo"></Image>
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">Bob Doe</div>
               <p className="text-gray-700 text-base">Marketing manager and content writer</p>
