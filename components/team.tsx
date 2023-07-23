787f6877f7d18736076a99ce163f1eb1cece0429
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-07-23 08:01:17 +0200

    Update team.tsx

--- components/team.tsx
+++ components/team.tsx
@@ -10,7 +10,7 @@ export default function TeamComponent(){
             <h1 className="text-4xl font-bold text-center text-white py-4">Meet Our Team</h1>
             <div className="flex flex-col md:flex-row gap-4 md:gap-0 flex-wrap justify-center">
            {/* <!-- Team member card --> */}
-          <div className="w-full md:w-1/4 bg-white rounded overflow-hidden shadow-lg md:m-4 flex flex-col justify-between">
+          <div className="w-full md:w-1/5 bg-white rounded overflow-hidden shadow-lg md:m-4 flex flex-col justify-between">
             <Image height={500} width={500} className="w-full rounded-full px-2 md:px-4" src={avatar} alt="Team member photo"></Image>
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">John Doe</div>
@@ -23,7 +23,7 @@ export default function TeamComponent(){
                     <a href="#" className="mx-2"><Image height={32} width={32} src={facebook} alt="Facebook icon"></Image></a>
             </div>
           </div>
-          <div className="w-full md:w-1/4 bg-white rounded overflow-hidden shadow-lg md:m-4 flex flex-col justify-between">
+          <div className="w-full md:w-1/5 bg-white rounded overflow-hidden shadow-lg md:m-4 flex flex-col justify-between">
             <Image height={500} width={500} className="w-full rounded-full px-2 md:px-4" src={avatar} alt="Team member photo"></Image>
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">Jane Doe</div>
@@ -36,7 +36,20 @@ export default function TeamComponent(){
                     <a href="#" className="mx-2"><Image height={32} width={32} src={facebook} alt="Facebook icon"></Image></a>
             </div>
           </div>
-          <div className="w-full md:w-1/4 bg-white rounded overflow-hidden shadow-lg md:m-4 flex flex-col justify-between">
+          <div className="w-full md:w-1/5 bg-white rounded overflow-hidden shadow-lg md:m-4 flex flex-col justify-between">
+            <Image height={500} width={500} className="w-full rounded-full px-2 md:px-4" src={avatar} alt="Team member photo"></Image>
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">Jane Doe</div>
+              <p className="text-gray-700 text-base">Data analyst and machine learning engineer</p>
+            </div>
+            <div className="px-6 py-4 flex justify-center">
+                    {/* <!-- Social icons --> */}
+                    <a href="#" className="mx-2"><Image height={32} width={32} src={twitter} alt="Twitter icon"></Image></a>
+                    <a href="#" className="mx-2"><Image height={32} width={32} src={linkedin} alt="LinkedIn icon"></Image></a>
+                    <a href="#" className="mx-2"><Image height={32} width={32} src={facebook} alt="Facebook icon"></Image></a>
+            </div>
+          </div>                
+          <div className="w-full md:w-1/5 bg-white rounded overflow-hidden shadow-lg md:m-4 flex flex-col justify-between">
             <Image height={500} width={500} className="w-full rounded-full px-2 md:px-4" src={avatar} alt="Team member photo"></Image>
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">Bob Doe</div>
