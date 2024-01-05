a1f99dd1eed2447ebec668eb0b97c9088cf3f9e5
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2024-01-05 11:26:05 +0000

    solved a typo on newsItem

--- components/newsItem.tsx
+++ components/newsItem.tsx
@@ -24,8 +24,8 @@ export default function NewsItem({ extensions }: any){
             </div>
             <div className="col-start-5 col-span-2 flex flex-col gap-4 pb-8 overflow-y-auto" id="sideLinks">
                 {extensions.map((ext: any, i: number) => (
-                    <Link href={`/extensions/1${i}`} passHref={true}>
-                        <div key={ext.key} className="grid grid-rows-6 grid-cols-2 relative text-white bg-green-900 hover:bg-green-600 active:bg-red-900 hover:shadow-2xl hover:shadow-sky-600">
+                    <Link key={ext.key} href={`/extensions/1${i}`} passHref={true}>
+                        <div className="grid grid-rows-6 grid-cols-2 relative text-white bg-green-900 hover:bg-green-600 active:bg-red-900 hover:shadow-2xl hover:shadow-sky-600">
                             <Image src={`https://reponse_backend-1-r0934826.deta.app/images/view/${ext.images[0]}`} alt="image" width={900} height={500} className="w-full col-span-full row-span-6 z-10"/>
                             <div className="w-full bg-opacity-80 col-span-full row-start-5 row-span-2 bg-inherit py-2 absolute bottom-0 z-20">
                                 <h1 className="text-center self-center cursor-pointer">{ext.title}</h1>
