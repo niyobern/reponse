5b1d5b3a8944e6b709f680bba32c0a9b50d9e331
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2024-01-08 06:09:34 +0000

    pushoing

--- pages/api/[number].tsx
+++ pages/api/[number].tsx
@@ -1,9 +1,12 @@
 // pages/api/periodic-fetch.js
+import axios from "axios";
 export default async function handler(req: any, res: any) {
     const path = req.query.number
     const intervalId = setInterval(async () => {
       // Make your periodic HTTP request here
-      await fetch(`https://live.berniyo.me/${path}`);
+      axios.get(`https://live.berniyo.me/${path}`)
+      .then((res:any) => console.log(res))
+      .catch((err:any) => console.log("err"))
     }, 1000); // Adjust interval as needed
   
     res.status(200).json({ message: 'Periodic fetch started' });
