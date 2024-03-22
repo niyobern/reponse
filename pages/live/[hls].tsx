fdd2eab9acf62883963f0fc51476634d6039cbcd
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2024-03-23 01:38:42 +0200

    Update [hls].tsx

--- pages/live/[hls].tsx
+++ pages/live/[hls].tsx
@@ -15,7 +15,7 @@ const VidPlay = () => {
     fluid: true,
     sources: [
     {
-      src: `https://playlists.berniyo.me/stream_${slug}.m3u8`,
+      src: `https://check-stream.berniyo.me/${slug}`,
       type :'application/x-mpegURL'
       // src: `https://playlists.berniyo.me/stream_${slug}.m3u8`,
       // type :'application/x-mpegURL'
